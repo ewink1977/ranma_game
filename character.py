85a73b1cb54d6eb4ed5ee35a683f270e39e49e84
Erin Winking <ewink@yahoo.com>
2021-01-04 04:09:59 -0800

    BATTLE FUNCTION WORKINGgit add .git add .

--- character.py
+++ character.py
@@ -2,16 +2,20 @@ import random
 
 # BASE CLASS ===
 class Character():
-    def __init__(self, name, hp, str, int, dex, per, level, xp = 0):
+    def __init__(self, name, hp, str, int, dex, per):
         self.name = name
         self.hp = hp
         self.str = str
         self.int = int
         self.dex = dex
         self.per = per
-        self.level = level
-        self.xp = xp
     
+    # def attack_roll(self, oppdex):
+    #     attackroll = random.randrange(1, 20, 1)
+    #     print(attackroll)
+    #     if attackroll > oppdex:
+    #         hp_hit = 
+
     def dodge_roll(self):
         dodge_roll = random.randrange(1, 20, 1)
         print(dodge_roll)
@@ -23,24 +27,57 @@ class Character():
 # PLAYER CHARACTER CLASSES ===
 class RanmaGirl(Character):
     def __init__(self):
-        super().__init__(name = 'Ranma-Chan', hp = 250, str = 7, int = 7, dex = 15, per = 11, level = 1)
+        super().__init__(name = 'Ranma-Chan', hp = 250, str = 7, int = 7, dex = 15, per = 11)
         self.ki = 10
+        self.kawaii = 2
+        self.pronoun = 'she'
 
 class RanmaBoy(Character):
     def __init__(self):
-        super().__init__(name = 'Ranma', hp = 300, str = 12, int = 8, dex = 8, per = 12, level = 1)
+        super().__init__(name = 'Ranma', hp = 300, str = 12, int = 8, dex = 8, per = 12)
         self.ki = 10
+        self.pronoun = 'he'
 
 class Akane(Character):
     def __init__(self):
-        super().__init__(name = 'Akane', hp = 225, str = 10, int = 9, dex = 9, per = 12, level = 1)
+        super().__init__(name = 'Akane', hp = 225, str = 10, int = 9, dex = 9, per = 12)
         self.ki = 0
+        self.pronoun = 'she'
+
+class Shampoo(Character):
+    def __init__(self):
+        super().__init__(name = 'Shampoo', hp = 250, str = 10, int = 8, dex = 11, per = 11)
+        self.ki = 0
+        self.kawaii = 5
+        self.pronoun = 'she'
 
 # NPC CHARACTER CLASSES ===
 class Tatewaki(Character):
     def __init__(self):
-        super().__init__(name = 'Kuno', hp = 250, str = 11, int = 6, dex = 11, per = 12, level = 1)
+        super().__init__(name = 'Kuno', hp = 250, str = 11, int = 6, dex = 11, per = 12)
+        self.ki = 0
+        self.pronoun = 'he'
+
+class Happosai(Character):
+    def __init__(self):
+        super().__init__(name = 'Happosai', hp = 400, str = 15, int = 8, dex = 13, per = 4)
+        self.ki = 20
+        self.pronoun = 'he'
+
+class Ryouga(Character):
+    def __init__(self):
+        super().__init__(name = 'Ryouga', hp = 500, str = 20, int = 4, dex = 8, per = 10)
         self.ki = 10
+        self.pronoun = 'he'
 
+class Mousse(Character):
+    def __init__(self):
+        super().__init__(name = 'Mousse', hp = 200, str = 5, int = 5, dex = 15, per = 15)
+        self.ki = 0
+        self.pronoun = 'he'
 
-
+class Henchman(Character):
+    def __init__(self):
+        super().__init__(name = 'Random Henchman', hp = 150, str = 10, int = 10, dex = 10, per = 10)
+        self.ki = 0
+        self.pronoun = 'he'
--- functions.py
+++ functions.py
@@ -8,4 +8,55 @@ def choose_character(choice):
         player = RanmaGirl()
     if choice == 3:
         player = Akane()
+    if choice == 4:
+        player = Shampoo()
     return player
+
+def battle(player, npc):
+    battleactive = True
+    may_flee = True
+    while battleactive == True:
+        print("\n")
+        print(f"{player.name}, your HP is {player.hp}.\n")
+        print(f"{npc.name}'s HP is at {npc.hp} \n")
+        print("What do you want to do?\n")
+        action = input(" A-Attack\n R-Run Away\n >>")
+        if action.capitalize() == 'A':
+            attack_roll = random.randrange(1, 20, 1)
+            if attack_roll > npc.dex:
+                hp_hit = attack_roll + player.str
+                npc.hp -= hp_hit
+                print(f"DEBUG: ROLL {attack_roll} - HP HIT {hp_hit} - NPC HP {npc.hp}")
+                if npc.hp > 0:
+                    print(f'YEOWCH! {npc.name} was just whomped for {hp_hit}!')
+                if npc.hp <= 0:
+                    print(f'Welp, you killed {npc.name}. I guess that makes you a good guy...')
+                    battleactive = False
+            else:
+                print(f"{npc.name} dodged your attack! Holy smokes!")
+            attack_roll = random.randrange(1, 20, 1)
+            if attack_roll > player.dex and battleactive == True:
+                print(f"{npc.name}'s turn now!")
+                hp_hit = attack_roll + npc.str
+                player.hp -= hp_hit
+                print(f"DEBUG: ROLL {attack_roll} - HP HIT {hp_hit} - PLAYER HP {player.hp}")
+                if player.hp > 0:
+                    print(f"KASPLOOSH!!! {npc.name} just clocked you for {hp_hit}!!")
+                if player.hp <= 0:
+                    print("Well, you're dead. Kind of sad. Bye bye.")
+                    battleactive = False
+            else:
+                print(f"You managed to dodge {npc.name}'s attack! Nice work!")
+        elif action.capitalize() == 'R':
+            if may_flee == True:
+                flee_roll = random.randrange(1, 20, 1)
+                if flee_roll <= player.dex:
+                    print(f"You managed to escape from {npc.name}!")
+                    battleactive = False
+                else:
+                    print("Flee failed. You have to fight.")
+                    may_flee = False
+            if may_flee == False:
+                print("YOU MAY NOT FLEE!")
+        else:
+            print('I gave you two freaking options...')
